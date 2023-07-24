5d0a5d58b791a5ed0c2136657e4858f05fe89737
TemoElizbarashvili <115801621+TemoElizbarashvili@users.noreply.github.com>
2023-07-24 22:56:20 +0400

    add recipe list, shopping list modules

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'Just Test','https://assets.epicurious.com/photos/5be1c1bf587ad42d4b37c134/1:1/w_640,c_limit/Vegetarian-Skillet-Stuffed-Shells-01112018.jpg'),
+    new Recipe('Khinkali', 'The best food in the world!', 'https://images.squarespace-cdn.com/content/v1/5e0a384845deef2566757c89/1583597752499-6CSD0FQZV9B30YFZNRVW/988A2004.jpg')
+  ];
+
+  constructor() { }
+}
